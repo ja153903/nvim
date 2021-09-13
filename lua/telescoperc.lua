b98acd6f7cac7b25cb5af5d57219a51be47d3d87
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-12 23:29:37 -0400

    Adding devicons

--- lua/telescoperc.lua
+++ lua/telescoperc.lua
@@ -1,20 +1,6 @@
 local telescope = require('telescope')
 
-telescope.setup {}
+telescope.setup {
+	disable_devicons = false,
+}
 
---telescope.setup {
-  --extensions={
-    --fzf = {
-      --fuzzy = true,
-      --override_generic_sorter = false,
-      --override_file_sorter = true,
-      --case_mode = "smart_case"
-    --},
-		--fzf_writer = {
-			--minimum_grep_characters = 3
-		--}
-  --}
---}
-
--- telescope.load_extension('fzf')
--- telescope.load_extension('fzf_writer')
