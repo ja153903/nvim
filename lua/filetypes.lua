ed83c801bd9fc23a21f8782aea80d6e73d24d062
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-20 17:31:06 -0400

    Solving problem in a more lua manner

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,74 +1,20 @@
-vim.cmd [[
-	autocmd FileType python set sw=4
-	autocmd FileType python set ts=4
-	autocmd FileType python set sts=4
+local fmt = string.format
+local filetype_configs = {
+	{ filename = "python", value = 4 },
+	{ filename = "html", value = 2 },
+	{ filename = "json", value = 2 },
+	{ filename = "yaml", value = 2 },
+	{ filename = "vim", value = 2 },
+	{ filename = "lua", value = 2 },
+	{ filename = "javascript", value = 2 },
+	{ filename = "javascriptreact", value = 2 },
+	{ filename = "typescript", value = 2 },
+	{ filename = "typescriptreact", value = 2 },
+}
+local fmt_settings = {"sw", "ts", "sts"}
 
-	autocmd FileType go set sw=4
-	autocmd FileType go set ts=4
-	autocmd FileType go set sts=4
-
-	autocmd FileType cpp set sw=2
-	autocmd FileType cpp set ts=2
-	autocmd FileType cpp set sts=2
-
-	autocmd FileType rust set sw=4
-	autocmd FileType rust set ts=4
-	autocmd FileType rust set sts=4
-
-	autocmd FileType html set sw=2
-	autocmd FileType html set ts=2
-	autocmd FileType html set sts=2
-
-	autocmd FileType json set sw=2
-	autocmd FileType json set ts=2
-	autocmd FileType json set sts=2
-
-	autocmd FileType yaml set sw=2
-	autocmd FileType yaml set ts=2
-	autocmd FileType yaml set sts=2
-
-	autocmd FileType vim set sw=2
-	autocmd FileType vim set ts=2
-	autocmd FileType vim set sts=2
-
-	autocmd FileType lua set sw=2
-	autocmd FileType lua set ts=2
-	autocmd FileType lua set sts=2
-
-	"Javascript spacing
-	autocmd FileType javascript set sw=2
-	autocmd FileType javascript set ts=2
-	autocmd FileType javascript set sts=2
-
-	"Typescript spacing
-	autocmd FileType typescript set sw=2
-	autocmd FileType typescript set ts=2
-	autocmd FileType typescript set sts=2
-
-	"JSX spacing
-	autocmd FileType javascriptreact set sw=2
-	autocmd FileType javascriptreact set ts=2
-	autocmd FileType javascriptreact set sts=2
-
-	"TSX spacing
-	autocmd FileType typescriptreact set sw=2
-	autocmd FileType typescriptreact set ts=2
-	autocmd FileType typescriptreact set sts=2
-
-	"Vue spacing
-	autocmd FileType vue set sw=2
-	autocmd FileType vue set ts=2
-	autocmd FileType vue set sts=2
-
-	autocmd FileType elm set sw=2
-	autocmd FileType elm set ts=2
-	autocmd FileType elm set sts=2
-
-	autocmd FileType svelte set sw=2
-	autocmd FileType svelte set ts=2
-	autocmd FileType svelte set sts=2
-
-	autocmd FileType lua set sw=2
-	autocmd FileType lua set ts=2
-	autocmd FileType lua set sts=2
-]]
+for _, config in ipairs(filetype_configs) do
+	for _, setting in ipairs(fmt_settings) do
+		vim.cmd(fmt("autocmd FileType %s set %s=%d", config.filename, setting, config.value))
+	end
+end
