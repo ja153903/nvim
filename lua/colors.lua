c89590237a6c485e068b7b098b18bde286d7d302
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-20 14:07:15 -0400

    refactoring to not use vim.opt instead of vim.cmd

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,9 +1,2 @@
-vim.cmd [[
-    if exists('+termguicolors')
-        let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-        let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-        set termguicolors
-    endif
-]]
-
+vim.opt.termguicolors = true
 vim.opt.background = 'dark'
