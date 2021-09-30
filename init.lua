8355512060e57f5d2d1e87005044a4afdbdb867b
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-30 11:23:40 -0400

    Adding comments

--- init.lua
+++ init.lua
@@ -5,7 +5,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd 'packadd packer.nvim'
 end
 
-require('packer').startup(function()
+require('packer').startup(function(use)
+	-- Essential plugins
   use 'wbthomason/packer.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'neovim/nvim-lspconfig'
@@ -16,36 +17,36 @@ require('packer').startup(function()
   use 'hoob3rt/lualine.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'glepnir/lspsaga.nvim'
-
+	-- Formatting
   use 'scrooloose/nerdcommenter'
   use 'sbdchd/neoformat'
-  
+  -- Telescope
   use 'nvim-lua/popup.nvim'
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-
+  -- git
   use 'airblade/vim-gitgutter'
   use 'f-person/git-blame.nvim'
-
+  -- javascript
   use 'yuezk/vim-js'
   use 'leafgarland/typescript-vim'
   use 'maxmellon/vim-jsx-pretty'
   use 'pantharshit00/vim-prisma'
   use 'jparise/vim-graphql'
-  
+  -- python 
   use 'davidhalter/jedi-vim'
   use 'cespare/vim-toml'
-
+  -- themes
   use 'EdenEast/nightfox.nvim'
-  
+  -- integrated terminal 
   use 'voldikss/vim-floaterm'
-
+  -- opt
   use 'lewis6991/impatient.nvim'
-
+  -- transparency
   use 'xiyaowong/nvim-transparent'
-
+  -- buffer
   use 'akinsho/bufferline.nvim'
 end)
 
