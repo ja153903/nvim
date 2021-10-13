ebf31bb12051629b0e018323ce3a51d2613bcb76
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-10-13 11:27:10 -0400

    Changing colorscheme to use palefox

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -66,7 +66,7 @@ require("bufferline").setup {
 
 local nightfox = require("nightfox")
 nightfox.setup {
-  fox = "nordfox", -- change the colorscheme to use nordfox
+  fox = "palefox", -- change the colorscheme to use nordfox
   styles = {
     comments = "italic", -- change style of comments to be italic
     keywords = "bold", -- change style of keywords to be bold
