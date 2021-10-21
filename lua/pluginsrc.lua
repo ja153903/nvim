ec508b77f9f8875432296678fc4e6806c5ba22dd
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-10-20 20:21:43 -0400

    updated prettier for formatting

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -40,6 +40,16 @@ require("formatter").setup(
             stdin = true
           }
         end
+      },
+      javascript = {
+        -- prettier
+        function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+            stdin = true
+          }
+        end
       }
     }
   }
@@ -252,7 +262,7 @@ lsp_installer.on_server_ready(
 vim.g.glow_binary_path = vim.env.HOME .. "/bin"
 
 -- toggleterm
-require("toggleterm").setup{
+require("toggleterm").setup {
   -- size can be a number or function which is passed the current terminal
   size = function(term)
     if term.direction == "horizontal" then
@@ -265,11 +275,11 @@ require("toggleterm").setup{
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
-  shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  shading_factor = "<number>", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   persist_size = true,
-  direction = 'float',
+  direction = "float",
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
   -- This field is only relevant if direction is set to 'float'
@@ -278,17 +288,17 @@ require("toggleterm").setup{
     -- see :h nvim_open_win for details on borders however
     -- the 'curved' border is a custom border type
     -- not natively supported but implemented in this plugin.
-    border = 'curved',
+    border = "curved",
     winblend = 3,
     highlights = {
       border = "Normal",
-      background = "Normal",
+      background = "Normal"
     }
   }
 }
 
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({cmd = "lazygit", hidden = true})
 
 function _lazygit_toggle()
   lazygit:toggle()
