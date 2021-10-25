8a3c25955e690f160ffcec8375080dd8134583d7
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-10-24 23:39:43 -0400

    reordered require statements

--- init.lua
+++ init.lua
@@ -97,10 +97,10 @@ require("packer").startup(
 )
 
 require("impatient")
+require("lspconfigrc")
 require("completionrc")
 require("settings")
 require("keymapping")
-require("lspconfigrc")
 require("filetypes")
 require("telescoperc")
 require("pluginsrc")
