a7157fef41523b02c0366382782e982e414d6980
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-10-27 14:03:18 -0400

    updating keybind for lazygit because it was causing lag in my typing

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -309,4 +309,4 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
