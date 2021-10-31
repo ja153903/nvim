e7cae5aaad34d9d74c0d3732a960e1b9cddf6431
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-10-31 15:58:27 -0400

    updating the lualine stuff

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -62,6 +62,10 @@ require("formatter").setup(
 
 -- LuaLine
 require("lualine").setup {
+  options = {
+    theme = "codedark",
+    section_separators = '',
+  },
   sections = {
     lualine_a = {"mode"},
     lualine_b = {"branch", "diff"},
