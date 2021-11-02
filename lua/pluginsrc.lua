56b0c6c936fa455c27c5fbfb3ce70b2381e53449
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-02 12:02:11 -0400

    not sure if this actually does anything

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -88,7 +88,7 @@ require("bufferline").setup {
 
 local nightfox = require("nightfox")
 nightfox.setup {
-  fox = "duskfox", -- change the colorscheme to use nordfox
+  fox = "nordfox", -- change the colorscheme to use nordfox
   styles = {
     comments = "italic", -- change style of comments to be italic
     keywords = "bold", -- change style of keywords to be bold
