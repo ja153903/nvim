634aa1eb03a2b7ef19180e733e88f65312caf6a3
ja153903 <jaimeabbariao@gmail.com>
2021-11-08 00:41:09 -0500

    updating some stuff

--- lua/lspconfigrc.lua
+++ lua/lspconfigrc.lua
@@ -122,43 +122,51 @@ rust_tools.setup(
   }
 )
 
-local sumneko_binary_path = vim.fn.exepath("lua-language-server")
-local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ":h:h:h")
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
 
-local runtime_path = vim.split(package.path, ";")
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-nvim_lsp.sumneko_lua.setup {
-  on_attach = on_attach,
-  capabilities = lsp_capabilities,
-  flags = {
-    debounce_text_changes = 150
-  },
-  cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"},
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
+        version = 'LuaJIT',
         -- Setup your lua path
-        path = runtime_path
+        path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {"vim"}
+        globals = {'vim'},
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true)
+        library = vim.api.nvim_get_runtime_file("", true),
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
-        enable = false
-      }
-    }
-  }
+        enable = false,
+      },
+    },
+  },
 }
+
 
 --nvim_lsp.ccls.setup(default_lsp_config)
 --nvim_lsp.solang.setup(default_lsp_config)
