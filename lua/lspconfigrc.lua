7951c019d440aae4c872e131fd762708c08354fd
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-12-02 21:33:25 -0500

    adding svelte

--- lua/lspconfigrc.lua
+++ lua/lspconfigrc.lua
@@ -175,3 +175,5 @@ nvim_lsp.graphql.setup(default_lsp_config)
 nvim_lsp.prismals.setup(default_lsp_config)
 nvim_lsp.html.setup(default_lsp_config)
 nvim_lsp.cssls.setup(default_lsp_config)
+nvim_lsp.svelte.setup{}
+
