c5bb53c1e45c837c3bc8eef6176b595633e658aa
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-12-28 10:56:53 -0500

    updating colorscheme

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -87,17 +87,17 @@ require("bufferline").setup {
   }
 }
 
---local nightfox = require("nightfox")
---nightfox.setup {
-  --fox = "nightfox", -- change the colorscheme to use nordfox
-  --styles = {
-    --comments = "italic", -- change style of comments to be italic
-    --keywords = "bold", -- change style of keywords to be bold
-    --functions = "italic,bold" -- styles can be a comma separated list
-  --},
-  --transparent = false
---}
---nightfox.load()
+local nightfox = require("nightfox")
+nightfox.setup {
+  fox = "nightfox", -- change the colorscheme to use nordfox
+  styles = {
+    comments = "italic", -- change style of comments to be italic
+    keywords = "bold", -- change style of keywords to be bold
+    functions = "italic,bold" -- styles can be a comma separated list
+  },
+  transparent = false
+}
+nightfox.load()
 
 require("dapui").setup()
 
