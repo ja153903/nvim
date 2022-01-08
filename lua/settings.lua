de368820d087d6d79f3ceb217acac1fe2dae6bb9
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-01-08 16:54:23 -0500

    adding some stuff

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,6 +13,7 @@ set.autoindent = true
 set.termguicolors = true
 set.background = "dark"
 
+vim.g.tokyonight_style = "night"
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_dark_float = false
 vim.cmd "colorscheme tokyonight"
