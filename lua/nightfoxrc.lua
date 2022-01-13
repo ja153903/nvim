64e464ab6e843a94ca8f2d0ce0d77c2cf5ad3d4c
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-01-12 21:57:29 -0500

    using nightfox again

--- lua/nightfoxrc.lua
+++ lua/nightfoxrc.lua
@@ -1,13 +1,13 @@
 local nightfox = require "nightfox"
 
 nightfox.setup {
-  fox = "dayfox",
+  fox = "nightfox",
   styles = {
     comments = "italic", -- change style of comments to be italic
     keywords = "bold", -- change style of keywords to be bold
     functions = "bold", -- styles can be a comma separated list
   },
-  transparent = false,
+  transparent = true,
 }
 
 nightfox.load()
