856c30ea78da33b0c39cab4baa7582bd8c2a8c30
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-02-08 10:33:47 -0500

    chore: running formatter

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -2,7 +2,7 @@ vim.cmd "autocmd BufEnter * :syntax sync fromstart"
 vim.cmd "autocmd BufEnter * :syntax sync clear"
 
 require("nvim-treesitter.configs").setup {
-  context_commentstring = {enable = true},
+  context_commentstring = { enable = true },
   ensure_installed = {
     "python",
     "javascript",
@@ -15,13 +15,13 @@ require("nvim-treesitter.configs").setup {
     "html",
     "json",
     "css",
-    "scss"
+    "scss",
   },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false
+    additional_vim_regex_highlighting = false,
   },
-  indent = {enable = false}
+  indent = { enable = false },
 }
 
 -- nvim-treesitter-context
@@ -38,19 +38,19 @@ require("treesitter-context").setup {
     default = {
       "class",
       "function",
-      "method"
+      "method",
       -- 'for', -- These won't appear in the context
       -- 'while',
       -- 'if',
       -- 'switch',
       -- 'case',
-    }
+    },
     -- Example for a specific filetype.
     -- If a pattern is missing, *open a PR* so everyone can benefit.
     --   rust = {
     --       'impl_item',
     --   },
-  }
+  },
 }
 
 require("headlines").setup {
@@ -59,37 +59,37 @@ require("headlines").setup {
     source_pattern_end = "^```$",
     dash_pattern = "^---+$",
     headline_pattern = "^#+",
-    headline_signs = {"Headline"},
+    headline_signs = { "Headline" },
     codeblock_sign = "CodeBlock",
-    dash_highlight = "Dash"
+    dash_highlight = "Dash",
   },
   rmd = {
     source_pattern_start = "^```",
     source_pattern_end = "^```$",
     dash_pattern = "^---+$",
     headline_pattern = "^#+",
-    headline_signs = {"Headline"},
+    headline_signs = { "Headline" },
     codeblock_sign = "CodeBlock",
-    dash_highlight = "Dash"
+    dash_highlight = "Dash",
   },
   vimwiki = {
     source_pattern_start = "^{{{%a+",
     source_pattern_end = "^}}}$",
     dash_pattern = "^---+$",
     headline_pattern = "^=+",
-    headline_signs = {"Headline"},
+    headline_signs = { "Headline" },
     codeblock_sign = "CodeBlock",
-    dash_highlight = "Dash"
+    dash_highlight = "Dash",
   },
   org = {
     source_pattern_start = "#%+[bB][eE][gG][iI][nN]_[sS][rR][cC]",
     source_pattern_end = "#%+[eE][nN][dD]_[sS][rR][cC]",
     dash_pattern = "^-----+$",
     headline_pattern = "^%*+",
-    headline_signs = {"Headline"},
+    headline_signs = { "Headline" },
     codeblock_sign = "CodeBlock",
-    dash_highlight = "Dash"
-  }
+    dash_highlight = "Dash",
+  },
 }
 
 local lsp_installer = require "nvim-lsp-installer"
@@ -99,25 +99,23 @@ lsp_installer.settings {
     icons = {
       server_installed = "✓",
       server_pending = "➜",
-      server_uninstalled = "✗"
-    }
-  }
+      server_uninstalled = "✗",
+    },
+  },
 }
 
-lsp_installer.on_server_ready(
-  function(server)
-    local opts = {}
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+  -- (optional) Customize the options passed to the server
+  -- if server.name == "tsserver" then
+  --     opts.root_dir = function() ... end
+  -- end
 
-    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-    server:setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
-  end
-)
+  -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
+  server:setup(opts)
+  vim.cmd [[ do User LspAttachBuffers ]]
+end)
 
 -- glow
 vim.g.glow_binary_path = vim.env.HOME .. "/bin"
@@ -153,19 +151,19 @@ require("toggleterm").setup {
     winblend = 3,
     highlights = {
       border = "Normal",
-      background = "Normal"
-    }
-  }
+      background = "Normal",
+    },
+  },
 }
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new {cmd = "lazygit", hidden = true}
+local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
 
 function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 -- dashboard-nvim
 vim.g.dashboard_default_executive = "telescope"
@@ -210,7 +208,7 @@ require("indent_blankline").setup {
   space_char_blankline = " ",
   show_current_context = true,
   show_current_context_start = true,
-  filetype_exclude = {"dashboard"}
+  filetype_exclude = { "dashboard" },
 }
 
 require("todo-comments").setup {}
