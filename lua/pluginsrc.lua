33bdae0c1881caf5fe6b3e18dc9691c2598ce8d6
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-02-12 01:39:33 -0500

    fix: adding back stuff for indent_blankline

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -201,8 +201,8 @@ let g:dashboard_custom_header = [
 require("nvim-autopairs").setup {}
 
 vim.opt.list = true
---vim.opt.listchars:append "space:⋅"
---vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
   space_char_blankline = " ",
