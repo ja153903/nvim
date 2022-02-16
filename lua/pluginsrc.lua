319cece289ada7d650b75edc18be1fcfdd8143dd
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-02-16 15:18:10 -0500

    fixing some things

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -201,7 +201,7 @@ require("nvim-autopairs").setup {}
 
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
---vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
   space_char_blankline = " ",
