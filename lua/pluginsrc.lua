3a2a81d62dc5905353a10df6a0321811887cd32e
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-03-25 22:02:31 -0400

    feat: updating plugin indentblankline

--- lua/pluginsrc.lua
+++ lua/pluginsrc.lua
@@ -200,11 +200,11 @@ let g:dashboard_custom_header = [
 require("nvim-autopairs").setup {}
 
 vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
+ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
-  --space_char_blankline = " ",
+  space_char_blankline = " ",
   show_current_context = true,
   show_current_context_start = true,
   filetype_exclude = { "dashboard" },
