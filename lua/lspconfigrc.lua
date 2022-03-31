5df47c5028ab9a1b30fc53565ba3f1a30c96fdfa
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-03-31 00:14:02 -0400

    feat: refactoring sumneko implementation

--- lua/lspconfigrc.lua
+++ lua/lspconfigrc.lua
@@ -138,26 +138,25 @@ rust_tools.setup {
 }
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_binary = vim.fn.exepath "lua-language-server"
-local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary, ":h:h:h")
+--local sumneko_binary = vim.fn.exepath "lua-language-server"
+--local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary, ":h:h:h")
 
-local runtime_path = vim.split(package.path, ";")
+local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require("lspconfig").sumneko_lua.setup {
-  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+require'lspconfig'.sumneko_lua.setup {
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
+        version = 'LuaJIT',
         -- Setup your lua path
         path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
+        globals = {'vim'},
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
