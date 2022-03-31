57840a040af369d138d09ebadda72c60c80e8224
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-03-31 00:16:39 -0400

    feat: updating kanagawa

--- lua/kanagawarc.lua
+++ lua/kanagawarc.lua
@@ -1,1 +1,18 @@
+require('kanagawa').setup({
+    undercurl = true,           -- enable undercurls
+    commentStyle = "NONE",
+    functionStyle = "bold",
+    keywordStyle = "bold",
+    statementStyle = "bold",
+    typeStyle = "bold",
+    variablebuiltinStyle = "bold",
+    specialReturn = true,       -- special highlight for the return keyword
+    specialException = true,    -- special highlight for exception handling keywords
+    transparent = false,        -- do not set background color
+    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+    globalStatus = false,       -- adjust window separators highlight for laststatus=3
+    colors = {},
+    overrides = {},
+})
+
 vim.cmd("colorscheme kanagawa")
