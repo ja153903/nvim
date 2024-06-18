3c559a9f55840f6b98495e9253cecab189f8fb74
ja153903 <jaimeabbariao@gmail.com>
2024-06-18 19:26:17 -0400

    updating lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,12 +3,11 @@ return {
   dependencies = { 'SmiteshP/nvim-navic' },
   event = 'VeryLazy',
   opts = function()
-    local colors = require('cyberdream.colors').default
     return {
       options = {
-        component_separators = { left = ' ', right = ' ' },
-        section_separators = { left = ' ', right = ' ' },
-        -- theme = cyberdream.get_theme,
+        theme = 'solarized_dark',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         globalstatus = true,
         disabled_filetypes = { statusline = { 'dashboard', 'alpha' } },
       },
@@ -37,34 +36,18 @@ return {
             cond = function()
               return package.loaded['nvim-navic'] and require('nvim-navic').is_available()
             end,
-            color = { fg = colors.grey, bg = colors.none },
           },
         },
         lualine_x = {
           {
             require('lazy.status').updates,
             cond = require('lazy.status').has_updates,
-            color = { fg = colors.green },
-          },
-          {
-            function()
-              local icon = ' '
-              local status = require('copilot.api').status.data
-              return icon .. (status.message or '')
-            end,
-            cond = function()
-              local ok, clients = pcall(vim.lsp.get_clients, { name = 'copilot', bufnr = 0 })
-              return ok and #clients > 0
-            end,
           },
           { 'diff' },
         },
         lualine_y = {
           {
             'progress',
-          },
-          {
-            'location',
           },
         },
         lualine_z = {
