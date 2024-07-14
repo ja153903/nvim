f9fcccb5f30ae3b3c17ad615d56d1b8255b4e43d
ja153903 <jaimeabbariao@gmail.com>
2024-07-14 15:39:59 -0400

    disabling which-key

--- init.lua
+++ init.lua
@@ -186,6 +186,7 @@ else
     },
     { -- Useful plugin to show you pending keybinds.
       'folke/which-key.nvim',
+      enabled = false,
       event = 'VimEnter', -- Sets the loading event to 'VimEnter'
       config = function() -- This is the function that runs, AFTER loading
         require('which-key').setup()
